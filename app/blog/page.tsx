b95facf21c41d2b563a8dcd5861a5f2b8828caef
vadimghedreutan <vadim.ghedreutan@icloud.com>
2023-07-12 19:09:23 +0200

    remove page animation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next"
 import Link from "next/link"
 import { allBlogs } from "contentlayer/generated"
-import ProviderAnimation from "../ProviderAnimation"
 export const metadata: Metadata = {
 	title: "Blog",
 	description: "Read my posts on web development, sysadmin, and more.",
-}
-
-export async function generateStaticParams() {
-	return allBlogs.map((post) => ({
-		slug: post.slug,
-	}))
 }
 
 const BlogPage = () => {
@@ -20,54 +13,49 @@ const BlogPage = () => {
 				Blog<span className="text-white">.</span>
 			</h1>
 
-			<ProviderAnimation>
-				<article className="flex flex-col gap-8 mt-12">
-					{allBlogs
-						.sort((a, b) => {
-							if (
-								new Date(a.publishedAt) >
-								new Date(b.publishedAt)
-							) {
-								return -1
-							}
-							return 1
-						})
-						.map((post) => (
-							<div
-								key={post.slug}
-								className="group relative flex flex-col items-start cursor-pointer"
-							>
-								<h2 className="text-base font-semibold tracking-tight">
-									<div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
-									<Link href={`/blog/${post.slug}`}>
-										<span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-										<span className="relative z-10">
-											{post.title}
-										</span>
-									</Link>
-								</h2>
-								<time className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-500 dark:text-zinc-600 pl-3.5">
-									{post.publishedAt}
-									<span
-										className="absolute inset-y-0 left-0 flex items-center"
-										aria-hidden="true"
-									>
-										<span className="h-4 w-0.5 rounded-full bg-zinc-300 dark:bg-zinc-700"></span>
+			<article className="flex flex-col gap-8 mt-12">
+				{allBlogs
+					.sort((a, b) => {
+						if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+							return -1
+						}
+						return 1
+					})
+					.map((post) => (
+						<div
+							key={post.slug}
+							className="group relative flex flex-col items-start cursor-pointer"
+						>
+							<h2 className="text-base font-semibold tracking-tight">
+								<div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+								<Link href={`/blog/${post.slug}`}>
+									<span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+									<span className="relative z-10">
+										{post.title}
 									</span>
-								</time>
-								<p className="relative z-10 mt-1 text-base text-zinc-600 dark:text-zinc-400">
-									{post.description}
-								</p>
-								<div
+								</Link>
+							</h2>
+							<time className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-500 dark:text-zinc-600 pl-3.5">
+								{post.publishedAt}
+								<span
+									className="absolute inset-y-0 left-0 flex items-center"
 									aria-hidden="true"
-									className="relative z-10 mt-3 flex items-center text-sm font-medium text-teal-500"
 								>
-									Read article
-								</div>
+									<span className="h-4 w-0.5 rounded-full bg-zinc-300 dark:bg-zinc-700"></span>
+								</span>
+							</time>
+							<p className="relative z-10 mt-1 text-base text-zinc-600 dark:text-zinc-400">
+								{post.description}
+							</p>
+							<div
+								aria-hidden="true"
+								className="relative z-10 mt-3 flex items-center text-sm font-medium text-teal-500"
+							>
+								Read article
 							</div>
-						))}
-				</article>
-			</ProviderAnimation>
+						</div>
+					))}
+			</article>
 		</div>
 	)
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import ProviderAnimation from "../ProviderAnimation"
 import ProjectCard from "components/ProjectCard"
 import { items } from "../../components/info"
 
@@ -16,24 +15,23 @@ const Projects = () => {
 					Projects<span className="text-white">.</span>
 				</h1>
 			</div>
-			<ProviderAnimation>
-				<div className="grid grid-cols-fluid sm:grid-cols-fluid_second px-5 sm:px-10 py-8 gap-4">
-					{items &&
-						items
-							.sort((a, b) => {
-								if (
-									new Date(a.publishedAt) >
-									new Date(b.publishedAt)
-								) {
-									return -1
-								}
-								return 1
-							})
-							.map((item) => (
-								<ProjectCard key={item.title} {...item} />
-							))}
-				</div>
-			</ProviderAnimation>
+
+			<div className="grid grid-cols-fluid sm:grid-cols-fluid_second px-5 sm:px-10 py-8 gap-4">
+				{items &&
+					items
+						.sort((a, b) => {
+							if (
+								new Date(a.publishedAt) >
+								new Date(b.publishedAt)
+							) {
+								return -1
+							}
+							return 1
+						})
+						.map((item) => (
+							<ProjectCard key={item.title} {...item} />
+						))}
+			</div>
 		</div>
 	)
 }
