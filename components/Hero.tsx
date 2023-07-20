24832ba0472fecdd89f1844d9ea84405971e6d7b
vadimghedreutan <vadim.ghedreutan@icloud.com>
2023-07-20 15:52:32 +0200

    add profile image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,30 +1,80 @@
 "use client"
-
+import { useState, useEffect } from "react"
+import clsx from "clsx"
 import { motion } from "framer-motion"
 import Image from "next/image"
 import { FaGithub, FaTwitter } from "react-icons/fa"
 import { MdEmail } from "react-icons/md"
 
-import me from "../app/profile_art.webp"
-
 const Hero = () => {
+	const [isLoading, setLoading] = useState(true)
 	return (
 		<div className="relative px-5 sm:px-10 w-full big:max-w-screen-2xl mx-auto">
 			<div className="mb-12">
 				<div className="w-full">
 					<div className="max-w-2xl flex flex-col space-y-6">
-						<Image
-							src={me}
-							alt="Vadim Ghedreutan"
-							className="rounded-full"
-							placeholder="blur"
-							width={120}
-							priority
-						/>
-
 						<h1 className="text-3xl font-bold tracking-tighter sm:text-4xl">
 							hey, I'm vadim 👋
 						</h1>
+						{/* grid gallery */}
+						<div className="columns-2 gap-4 sm:w-4/5">
+							<div className="relative h-40 mb-4">
+								<Image
+									src="/profile/img_0166.webp"
+									alt="vadim ghedreutan"
+									fill
+									sizes="100vw"
+									priority
+									className={clsx(
+										"object-cover rounded-lg",
+										isLoading ? "grayscale" : "grayscale-0"
+									)}
+									onLoadingComplete={() => setLoading(false)}
+								/>
+							</div>
+							<div className="relative h-80 sm:mb-0 ">
+								<Image
+									src="/profile/profile_600_800.webp"
+									alt="vadim ghedreutan"
+									fill
+									sizes="100vw"
+									priority
+									className={clsx(
+										"object-cover rounded-lg grid-areas-[2/1/4/2]",
+										isLoading ? "grayscale" : "grayscale-0"
+									)}
+									onLoadingComplete={() => setLoading(false)}
+								/>
+							</div>
+							<div className="relative h-80 mb-4 ">
+								<Image
+									src="/profile/img_0205.webp"
+									alt="vadim ghedreutan"
+									fill
+									sizes="100vw"
+									priority
+									className={clsx(
+										"object-cover rounded-lg",
+										isLoading ? "grayscale" : "grayscale-0"
+									)}
+									onLoadingComplete={() => setLoading(false)}
+								/>
+							</div>
+							<div className="relative h-40 sm:mb-0  ">
+								<Image
+									src="/profile/img_0270.webp"
+									alt="vadim ghedreutan"
+									fill
+									sizes="100vw"
+									priority
+									className={clsx(
+										"object-cover rounded-lg",
+										isLoading ? "grayscale" : "grayscale-0"
+									)}
+									onLoadingComplete={() => setLoading(false)}
+								/>
+							</div>
+						</div>
 						<p className="prose mt-6 text-zinc-600 dark:text-zinc-400 pb-2">
 							I'm a System Administrator, Web Developer,
 							proficient in troubleshooting issues, operating
