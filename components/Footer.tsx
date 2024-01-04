646cd4db277d5dbda3294971f85970bd11751e8e
vadimghedreutan <vadim.ghedreutan@icloud.com>
2024-01-04 18:48:55 +0100

    refactor: align footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
 	return (
 		<section className="py-12 px-5 sm:px-10 big:max-w-screen-2xl mx-auto">
-			<div className="flex">
-				<p className="text-sm">@2023 - Design and development by me.</p>
+			<div className="flex flex-col items-center">
+				<p className="text-sm">- Design and development by me.</p>
+				<p className="text-sm">@2024</p>
 			</div>
 		</section>
 	)
