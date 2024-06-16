ba6524071bd5903b8db393be12abc041789cc340
vadimghedreutan <vadim.ghedreutan@icloud.com>
2024-06-16 21:46:55 +0200

    Add 404

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+	return (
+		<section className="relative px-5 sm:px-2 w-full max-w-2xl mx-auto">
+			<div className="flex items-center justify-center">
+				<h1 className="font-medium text-2xl mb-8 tracking-tighter">
+					Oh no! This page doesn't exist.
+				</h1>
+			</div>
+		</section>
+	)
+}
